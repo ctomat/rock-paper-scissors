9a0ac20ee8f2da7fe39da5a69d697b75275808be
Cristian <ctomatfl@gmail.com>
2022-09-20 19:07:54 -0400

    Generate computer choices in the game

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+/* The constant is initialized with the three possible options. 
+Since it will be immutable, it is written in capital letters. */
+const ROCK_PAPER_SCISSORS = ["rock", "paper", "scissors"];
 
+function getComputerChoice() {
+  return ROCK_PAPER_SCISSORS[
+    Math.floor(Math.random() * ROCK_PAPER_SCISSORS.length)
+  ];
+}
+
+function onClickStartButton() {
+  console.log(getComputerChoice());
+}
