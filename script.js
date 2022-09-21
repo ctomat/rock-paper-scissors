00fdaf9616cd5632d7bf393dc18fa93bb52ba38d
Cristian <ctomatfl@gmail.com>
2022-09-21 12:37:22 -0400

    Create the game function to play five rounds

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 /* The constant is initialized with the three possible options. 
 Since it will be immutable, it is written in capital letters. */
 const ROCK_PAPER_SCISSORS = ["rock", "paper", "scissors"];
+const TIE_MESSAGE = "Tie!";
+const WIN_MESSAGE = "You win!";
+const LOST_MESSAGE = "You lose!";
+const INVALID_OPTION = "The option you chose is not valid";
 
 function getComputerChoice() {
   return ROCK_PAPER_SCISSORS[
@@ -9,11 +13,6 @@ function getComputerChoice() {
 }
 
 function playRound(computerSelection, playerSelection) {
-  const TIE_MESSAGE = "Tie!";
-  const WIN_MESSAGE = "You win!";
-  const LOST_MESSAGE = "You lose!";
-  const INVALID_OPTION = "The option you chose is not valid";
-
   const MATCH_CASES = {
     rock: () => {
       if (computerSelection === playerSelection) {
@@ -49,9 +48,44 @@ function playRound(computerSelection, playerSelection) {
     : INVALID_OPTION;
 }
 
+function game() {
+  const GAME_ROUNDS = 5;
+  let playerWins = 0;
+  let computerWins = 0;
+
+  const ROUND_WINNER = {
+    [WIN_MESSAGE]: () => playerWins++,
+    [LOST_MESSAGE]: () => computerWins++,
+  };
+
+  function gameWinner() {
+    if (playerWins > computerWins) {
+      return "Player wins!";
+    } else if (playerWins < computerWins) {
+      return "Computer wins!";
+    } else {
+      return `It's a tie!`;
+    }
+  }
+
+  for (let round = 1; round <= GAME_ROUNDS; round++) {
+    const computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    const playerSelection = prompt(
+      "Write down your choice; it can be rock, paper or scissors."
+    );
+
+    const roundMessage = playRound(computerSelection, playerSelection);
+
+    ROUND_WINNER[roundMessage]();
+
+    console.log(roundMessage);
+  }
+
+  console.log(playerWins, computerWins);
+  console.log(gameWinner());
+}
+
 function onClickStartButton() {
-  const playerSelection = prompt(
-    "Write down your choice; it can be rock, paper or scissors"
-  );
-  console.log(playRound(computerSelection, playerSelection));
+  game();
 }
